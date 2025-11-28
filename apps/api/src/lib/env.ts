505fa86aa896ee00e105b616bcb8367e5ee2c79b
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-11-28 21:25:10 +0100

    feat: update api auth setup with github auth

--- apps/api/src/lib/env.ts
+++ apps/api/src/lib/env.ts
@@ -10,6 +10,8 @@ const EnvSchema = z.object({
   BETTER_AUTH_URL: z.url(),
   GOOGLE_CLIENT_ID: z.string().min(1),
   GOOGLE_CLIENT_SECRET: z.string().min(1),
+  GITHUB_CLIENT_ID: z.string().min(1),
+  GITHUB_CLIENT_SECRET: z.string().min(1),
 });
 
 export type Env = z.infer<typeof EnvSchema>;
