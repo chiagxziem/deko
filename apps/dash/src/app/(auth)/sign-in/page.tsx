9a7dafa0826ecd7b4fcefff4b6bb1c705b4bcf93
Chiagoziem Sunday <chiagoziem.ms@gmail.com>
2025-12-15 18:20:05 +0100

    refactor: update button styles

--- apps/dash/src/app/(auth)/sign-in/page.tsx
+++ apps/dash/src/app/(auth)/sign-in/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 const SignInPage = () => {
   return (
     <main className="flex flex-col gap-8">
-      <div className="flex flex-col items-center gap-2 text-center">
+      <div className="flex flex-col items-center gap-1.5 text-center">
         <h1 className="font-jetbrains font-semibold">SIGN IN TO LOGR</h1>
         <p className="text-muted-foreground text-sm">
           Welcome back! Please sign in to your account.
@@ -11,21 +11,11 @@ const SignInPage = () => {
       </div>
 
       <div className="flex flex-col gap-2">
-        <Button size={"lg"}>Default</Button>
         <Button size={"lg"} variant={"outline"}>
-          Outline
+          Continue with GitHub
         </Button>
-        <Button size={"lg"} variant={"secondary"}>
-          Secondary
-        </Button>
-        <Button size={"lg"} variant={"ghost"}>
-          Ghost
-        </Button>
-        <Button size={"lg"} variant={"destructive"}>
-          Destructive
-        </Button>
-        <Button size={"lg"} variant={"link"}>
-          Link
+        <Button size={"lg"} variant={"outline"}>
+          Continue with Google
         </Button>
       </div>
     </main>
