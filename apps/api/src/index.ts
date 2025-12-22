7ee4f6033dc6a21f0d2c95ed896f821a45e76959
Chiagoziem Sunday <chiagoziem.ms@gmail.com>
2025-12-22 18:44:52 +0100

    chore: rename rpc app to api app

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,18 +1,15 @@
-import createApp from "@/lib/create-app";
-import configureOpenAPI from "@/lib/openapi";
-import healthRouter from "@/routes/health/health.index";
-import ingestRouter from "@/routes/ingest/ingest.index";
-import projectsRouter from "@/routes/projects/projects.index";
-import userRouter from "@/routes/user/user.index";
+import { createApp } from "@/app";
+import health from "@/services/health/health.route";
+import ingest from "@/services/ingest/ingest.route";
+import project from "@/services/project/project.route";
+import user from "@/services/user/user.route";
 
 const app = createApp();
 
-const routers = [userRouter, healthRouter, ingestRouter, projectsRouter];
-
-configureOpenAPI(app);
-
-routers.forEach((router) => {
-  app.route("/api", router);
-});
+app
+  .route("/health", health)
+  .route("/user", user)
+  .route("/ingest", ingest)
+  .route("/project", project);
 
 export default app;
--- apps/api/src/middleware/not-found-route.ts
+++ apps/api/src/middleware/not-found-route.ts
@@ -1,13 +1,11 @@
 import type { NotFoundHandler } from "hono";
 
-import HttpStatusCodes from "@/utils/http-status-codes";
-
 const notFoundRoute: NotFoundHandler = (c) => {
   return c.json(
     {
       message: `Route not found - '${c.req.path}'`,
     },
-    HttpStatusCodes.NOT_FOUND,
+    404,
   );
 };
 
